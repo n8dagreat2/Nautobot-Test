2324e0f25f0522bc233d93be69e1f6a0dc5d4ae4
n8dagreat2 <nathaniel.c.ferre@gmail.com>
2024-06-10 11:03:02 -0700

    Add __init__ file and test code

---
+++ Jobs/change_port.py
@@ -0,0 +1,23 @@
+from nautobot.apps.jobs import Job, StringVar, register_jobs
+
+
+class ChangePorts(Job):
+    var1 = StringVar()
+
+    class Meta:
+        name = "Glorbus"
+        description = """
+            This job does a number of interesting things.
+            
+            1. It takes your kids to work
+            2. It sets your coordinates for a jump to hyperspace
+            3. It codes for you
+        """
+
+    def run(self, var1):
+        if var1 != "Star Wars is the best!":
+            self.logger.error("var1 must be 'Star Wars is the best!'")
+            raise Exception("Argument input validation failed.")
+
+
+register_jobs(ChangePorts)
